2b2254437568e078bd17fed5a28d3c51419c7648
lolagou <147620896+lolagou@users.noreply.github.com>
2025-04-03 18:20:06 -0300

    agrego contenedores

--- javascript.js
+++ javascript.js
@@ -1,9 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     let container = document.querySelector(".box-derecha");
+    let container2 = document.querySelector(".box-izquierda");
 
     for (let i = 0; i < 3; i++) {
         let div = document.createElement("div");
         div.classList.add("cuadrado-pequeño");
         container.appendChild(div);
+    }
+
+    for (let i = 0; i < 9; i++) {
+        let div = document.createElement("div");
+        div.classList.add("texto-pequeño");
+        container.appendChild(div);
+    }
+
+    for (let i = 0; i < 2; i++) {
+        let div = document.createElement("div");
+        div.classList.add("texto-img");
+        container2.appendChild(div);
     }
 });
